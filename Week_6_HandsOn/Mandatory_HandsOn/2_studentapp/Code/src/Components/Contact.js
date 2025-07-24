9fd0e94a222bc780e41e9e582ba6f39d4618c6dc
HIMASRI-MUVVA <167002965+HIMASRI-MUVVA@users.noreply.github.com>
2025-07-24 21:41:44 +0530

    Add files via upload

---
+++ Week_6_HandsOn/Mandatory_HandsOn/2_studentapp/Code/src/Components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h3> Welcome to the contact page of Student Portal</h3>
+    </div>
+  )
+}
+
+export default Contact
---
+++ Week_6_HandsOn/Mandatory_HandsOn/2_studentapp/Code/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Welcome To the home page of Student portal</h3>
+    </div>
+  )
+}
+
+export default Home
